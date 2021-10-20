1cc9448ddbb310c3602f1f768ffc73b2665705d8
Stackbit Code Editor <projects@stackbit.com>
2021-10-20 20:40:36 +0000

    SectionPricing.js: changed

--- src/components/SectionPricing.js
+++ src/components/SectionPricing.js
@@ -44,7 +44,7 @@ function PricingPlan(plan) {
         <div className="cell plan">
             <div className={classNames('card', { highlight: highlight })}>
                 <div className="plan-header">
-                    {title && <h3 className="plan-title">{title}</h2>}
+                    {title && <h2 className="plan-title">{title}</h2>}
                     {subtitle && <div className="plan-subtitle">{subtitle}</div>}
                     {price && <div className="plan-price">{price}</div>}
                 </div>
