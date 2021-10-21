1ddf5247ebddb7dcfaf66dc0e6567f2c0b65eeb3
Stackbit Code Editor <projects@stackbit.com>
2021-10-21 19:11:49 +0000

    Banner.js: changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 export default function Banner(props) {
-    const action = _.get(props, 'action');
+    // const action = _.get(props, 'action');
     
     return (
         <div>
