921d534d3643fa093378be066ca97df97dd13948
Stackbit Code Editor <projects@stackbit.com>
2021-10-26 14:18:56 +0000

    Banner.js: changed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Banner(props) {
     const headline = (props.section.headline);
-    
+    const actionButton 
     return (
         <div>
             <p>{headline}</p>
