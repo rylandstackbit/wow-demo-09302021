bf31a7e2fc84fd9459240dea72dc0b05313b6b70
Stackbit Code Editor <projects@stackbit.com>
2021-11-02 17:19:03 +0000

    SectionPricing.js: changed

--- src/components/SectionPricing.js
+++ src/components/SectionPricing.js
@@ -41,7 +41,7 @@ function PricingPlan(plan) {
     const actions = _.get(plan, 'actions');
     return (
         <div className="c-card-section">
-
+            Hello
         </div>
     )
     return (
